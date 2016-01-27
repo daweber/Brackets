46b620f9a18724c4ef9e54ad65e65e92e7c1d070
Matthew Weber <moworldenterprises@gmail.com>
2016-01-27 12:30:37 -0600

    refactor generated code

--- tv/src/main/java/com/daweber/brackets/GameDetailsCard.java
+++ tv/src/main/java/com/daweber/brackets/GameDetailsCard.java
@@ -89,7 +89,7 @@ public class GameDetailsCard extends DetailsFragment {
             updateBackground(mSelectedMovie.getBackgroundImageUrl());
             setOnItemViewClickedListener(new ItemViewClickedListener());
         } else {
-            Intent intent = new Intent(getActivity(), MainActivity.class);
+            Intent intent = new Intent(getActivity(), LeanBackBrackets.class);
             startActivity(intent);
         }
     }
